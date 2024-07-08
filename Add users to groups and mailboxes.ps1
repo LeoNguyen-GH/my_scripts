17284576a0cea3413068fae38e2938bf93df2e19
LeoNguyen-GH <leonguyen.ln1@gmail.com>
2024-07-08 20:09:30 +0100

    Update Add users to groups and mailboxes.ps1

--- Add users to groups and mailboxes.ps1
+++ Add users to groups and mailboxes.ps1
@@ -1,31 +1,37 @@
 . "$PSScriptRoot\common\functions.ps1"
 
-function add_group_members {
-    [CmdletBinding()]
+function add_group_member {
     param (
         [parameter(Mandatory, ValueFromPipeline)] [string]$user,
         [parameter(Mandatory)] [string]$group,
-        [parameter(Mandatory)] [string]$group_type
+        [bool]$display_total = $false
     )
     
-    process {
-        switch ($group_type) {
-            "distribution list" { Add-DistributionGroupMember -Identity $group -Member $user -BypassSecurityGroupManagerCheck -Confirm:$false }
-            "Microsoft 365 group" { Add-UnifiedGroupLinks -Identity $group -LinkType Members -Links $user -ErrorAction Stop }
-            "security group" { Add-AzureADGroupMember -ObjectId $(Get-AzureADGroup -SearchString $group).ObjectId -RefObjectId $(Get-AzureADUser -ObjectId $user).ObjectId }
-            { $_ -in @("user mailbox", "RoomMailbox", "shared mailbox") } {
-                Add-MailboxPermission -Identity $group -User $user -AccessRights FullAccess -InheritanceType All -AutoMapping $true
-                Add-RecipientPermission -Identity $group -Trustee $user -AccessRights SendAs -Confirm:$false }
-            "dynamic distribution list" {
-                write-host "Can't assign members to a dynamic distribution list '$group'" -ForegroundColor -Red
-                return }
-            default {
-                write-host "Group '$group' could not be found." -foregroundcolor Red
-                return }
-        }
-        
-        Write-Host "Added '$user' -> $group_type '$group'" -ForegroundColor Green
+    begin {
+        $users_added = 0
+        $group_type = group_type -email $group
+
+        if (!$group_type) { throw "Couldn't find group '$group'." }
+        elseif ($group_type -eq "dynamic distribution list") { throw "Cannot assign members to a dynamic distribution list: '$group'" }
+        if ($group_type -in @("security group", "Microsoft 365 group")) { $group_ID = $(Get-AzureADGroup -SearchString $group).ObjectId }
     }
+    
+    process {
+        try {
+            switch ($group_type) {
+                "distribution list" { Add-DistributionGroupMember -Identity $group -Member $user -BypassSecurityGroupManagerCheck -Confirm:$false -ErrorAction stop }
+                { $_ -in @("security group", "Microsoft 365 group") } { Add-AzureADGroupMember -ObjectId $group_ID -RefObjectId $(Get-AzureADUser -ObjectId $user).ObjectId }
+                { $_ -in @("user mailbox", "RoomMailbox", "shared mailbox") } {
+                    Add-MailboxPermission -Identity $group -User $user -AccessRights FullAccess -InheritanceType All -AutoMapping $true
+                    Add-RecipientPermission -Identity $group -Trustee $user -AccessRights SendAs -Confirm:$false }
+            }
+        
+            $users_added++
+            Write-Host "Added '$user' -> $group_type '$group'" -ForegroundColor Green
+        } catch { write-warning $_ }
+    }
+
+    end { if ($display_total) {Write-Host "Total users added - $group`: $users_added"} }
 }
 
 function main {
@@ -34,12 +40,11 @@ function main {
         return
     }
     
-    multi_user_input -prompt "Enter the group/mailbox email to add users to (Newline for multiple, enter 'q' to continue):" | ForEach-Object {
-        $group_type = group_type -email $_
-        $users | add_group_members -group $_ -group_type $group_type
+    $groups = multi_user_input -prompt "Enter the group/mailbox email to add users to (Newline for multiple, enter 'q' to continue):"
+
+    foreach ($group in $groups) {
+        try { $users | add_group_member -group $group -display_total $true } catch { write-warning $_ }
     }
-    
-    print_dash_across_terminal
 }
 
 EXO_connect
