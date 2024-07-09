c849436e18b1137c4ecf56ba4a355f9a5de2b76b
LeoNguyen-GH <leonguyen.ln1@gmail.com>
2024-07-09 21:44:57 +0100

    Update Get group or mailbox members.ps1

--- Get group or mailbox members.ps1
+++ Get group or mailbox members.ps1
@@ -1,11 +1,13 @@
 . "$PSScriptRoot\common\functions.ps1"
 
-function get_members {
+function get_group_members {
     param ([parameter(Mandatory)] [string]$group)
 
     try {
-        return $members = switch (group_type -email $group) {
-            { $_ -in @("security group", "Microsoft 365 group", "distribution list") } { Get-AzureADGroupMember -ObjectId (Get-AzureADGroup -SearchString $group).ObjectId -All:$true | Select-Object @{Name='Name'; Expression={$_.DisplayName}}, @{Name='Email'; Expression={$_.UserPrincipalName}} }
+        return $members = switch (get_group_type -email $group) {
+            "distribution list" {Get-DistributionGroupMember -Identity $group -ResultSize unlimited | Select-Object @{Name='Name'; Expression={$_.DisplayName}}, @{Name='Email'; Expression={$_.PrimarySmtpAddress}}}
+            "Microsoft 365 group" {Get-UnifiedGroupLinks -Identity $group -LinkType Members -ResultSize Unlimited -ErrorAction SilentlyContinue | Select-Object @{Name='Name'; Expression={$_.DisplayName}}, @{Name='Email'; Expression={$_.PrimarySmtpAddress}}}
+            "security group" {Get-AzureADGroupMember -ObjectId (Get-AzureADGroup -SearchString $group).ObjectId -All:$true -ErrorAction SilentlyContinue | Select-Object @{Name='Name'; Expression={$_.DisplayName}}, @{Name='Email'; Expression={$_.UserPrincipalName}}}
             "dynamic distribution list" { Get-Recipient -RecipientPreviewFilter (Get-DynamicDistributionGroup -Identity $group -ErrorAction SilentlyContinue).RecipientFilter | Select-Object @{Name='Name'; Expression={$_.DisplayName}}, @{Name='Email'; Expression={$_.PrimarySmtpAddress}} }
             { $_ -in @("RoomMailbox", "shared mailbox", "user mailbox") } { Get-MailboxPermission -Identity (Get-Mailbox -Identity $group) | Where-object { $_.User -notlike "*SELF*" } | Select-Object @{Name='Name'; Expression={$_.User}},AccessRights }
             default { throw "Could not find the group '$group'." }
@@ -14,11 +16,13 @@ function get_members {
 }
 
 function main {
-    $groups = multi_user_input -prompt "Enter the groups and mailboxes email to parse (Newline for multiple, enter 'q' to continue):"
+    $groups = get_multi_user_input -prompt "Enter the groups and mailboxes email to parse (Newline for multiple, enter 'q' to continue):"
     
     foreach ($group in $groups) {
-        if ($members = get_members -group $group) {
+        if ($members = get_group_members -group $group) {
             export_csv -csv_file_path "$PSScriptRoot\_logs\exported\$group - list of members $(Get-Date -Format 'dd-MM-yyyy h.mmtt').csv" -data $members
+        } else {
+            write-output "No members found for '$group'."
         }
     }
 }
@@ -26,6 +30,4 @@ function main {
 EXO_connect
 azureAD_connect
 
-while ($true) {
-    main
-}
+while ($true) { main }
