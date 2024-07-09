bca69f78ac28a6a5faaa63ceea69fbbdc41271bb
LeoNguyen-GH <leonguyen.ln1@gmail.com>
2024-07-09 21:49:13 +0100

    Update Get groups an email is member of.ps1

--- Get groups an email is member of.ps1
+++ Get groups an email is member of.ps1
@@ -1,55 +1,44 @@
 . "$PSScriptRoot\common\functions.ps1"
+get_functions -file_path "$PSScriptRoot\Get group or mailbox members.ps1" | Invoke-Expression
 
-$dir_path = "$PSScriptRoot\_logs\exported\all groups"
+function export_all_groups {
+    # Removes the all groups folder then gets all the groups in the tenent and exports it to a csv file
+    param ([parameter(Mandatory)] [string]$dir_path)
 
-function get_members {
-    param ([parameter(Mandatory)] [string]$group)
-    
-    return $members = switch ($group_type = group_type -email $group) {
-        "distribution list" { Get-DistributionGroupMember -Identity $group -ResultSize unlimited | Select-Object @{Name='Name'; Expression={$_.DisplayName}}, @{Name='Email'; Expression={$_.PrimarySmtpAddress}} }
-        "Microsoft 365 group" { Get-UnifiedGroupLinks -Identity $group -LinkType Members -ResultSize Unlimited -ErrorAction SilentlyContinue | Select-Object @{Name='Name'; Expression={$_.DisplayName}}, @{Name='Email'; Expression={$_.PrimarySmtpAddress}} }
-        "security group" { Get-AzureADGroupMember -ObjectId (Get-AzureADGroup -SearchString $group).ObjectId -All:$true | Select-Object @{Name='Name'; Expression={$_.DisplayName}}, @{Name='Email'; Expression={$_.UserPrincipalName}} }
-        "dynamic distribution list" { Get-Recipient -RecipientPreviewFilter (Get-DynamicDistributionGroup -Identity $group -ErrorAction SilentlyContinue).RecipientFilter | Select-Object @{Name='Name'; Expression={$_.DisplayName}}, @{Name='Email'; Expression={$_.PrimarySmtpAddress}} }
-        { $group_type -in @("RoomMailbox", "shared mailbox", "user mailbox") } { Get-MailboxPermission -Identity (Get-Mailbox -Identity $group) | Where-object { $_.User -notlike "*SELF*" } | Select-Object @{Name='Name'; Expression={$_.User}},AccessRights }
-        default { return $null }
-    }
-}
-
-function populate_groups {
     Remove-Item -Path $dir_path -Force -Recurse -ErrorAction SilentlyContinue
 
-    # Loop through each group
-    Get-AzureADGroup -All:$true | select-object DisplayName, Mail, ObjectId, @{Name='ProxyAddresses'; Expression={($_.ProxyAddresses -join ';')}}, MailEnabled, SecurityEnabled | ForEach-Object {
-        if (!($group = $_.Mail)) {
-            $group = $_.DisplayName
-        }
+    $groups = Get-AzureADGroup -All:$true | select-object DisplayName, Mail, ObjectId, @{Name='ProxyAddresses'; Expression={($_.ProxyAddresses -join ';')}}, MailEnabled, SecurityEnabled
+    foreach ($group in $groups) {
+        Write-Progress -Activity "Exporting in progress" -Status "$($group / $groups.Count * 100)% Complete:" -PercentComplete ($group / $groups.Count * 100)
         
-        # Get members of each group
-        if(!($members = get_members -group $group)) {
+        $group_name = if ($group.Mail) { $group.Mail } else { $group.DisplayName }
+        
+        if(!($members = get_group_members -group $group_name)) {
             $members = [PSCustomObject]@{
                 name = "" 
                 email = ""
             }
         }
 
-        # Export members of each group to a CSV file
-        export_csv -data $members -csv_file_path "$dir_path\$group.csv"
+        export_csv -data $members -csv_file_path "$dir_path\$group_name.csv"
     }
 }
 
 function find_members_of {
     param (
-        [array]$members
+        [array]$members,
+        [parameter(Mandatory)] [string]$dir_path
     )
     
     $members_of = @{}
     
-    get-childitem -Path $dir_path -Filter *.csv | ForEach-Object {
-        $filename_without_extension = $_.BaseName
+    $csv_files = get-childitem -Path $dir_path -Filter *.csv
+    foreach ($file in $csv_files) {
+        $filename_without_extension = $file.BaseName
         
         # Add to the hashtable with the matched members in the csv file being the key and the key value being the csv group filename, the key value is a array to sort multiple groups the member key is a member of e.g. {"user@email.com" = @("group1", "group2")}
-        $csv_data = Import-Csv -Path $_.FullName
-
+        $csv_data = Import-Csv -Path $file.FullName
+            
         foreach ($row in $csv_data) {
             if ($row.email -in $members) {
                 $members_of[$row.email] += @($filename_without_extension)
@@ -82,14 +71,14 @@ function generate_csv_member_of {
 }
 
 function main {
-    $emails = multi_user_input -prompt "Enter the groups/users email to find which group they are a member of (Newline for multiple, enter 'q' to continue):"
+    $emails = get_multi_user_input -prompt "Enter the groups/users email to find which group they are a member of (Newline for multiple, enter 'q' to continue):"
 
-    if (closed_input -user_prompt "Do you want to repopulate the 'all groups' folder with all email groups and its respective members? (Y/N)" -y_msg "Repopulating 'all groups' folder.") {
-        populate_groups
+    if (closed_input -user_prompt "Do you want to refresh the cache 'all groups' folder with all email groups and its respective members? (Y/N)" -y_msg "Refreshing cache.") {
+        export_all_groups -dir_path "$PSScriptRoot\_logs\cache\all groups"
     }
     
     # Generate the CSV files with the emails the inputs are a member of
-    if ($members_of = find_members_of -members $emails) {
+    if ($members_of = find_members_of -members $emails -dir_path "$PSScriptRoot\_logs\cache\all groups") {
         generate_csv_member_of -members_of $members_of
         $members_of
     }
@@ -98,6 +87,4 @@ function main {
 EXO_connect
 azureAD_connect
 
-while ($true) {
-    main
-}
+while ($true) { main }
