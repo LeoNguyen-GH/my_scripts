cb8d237d512963da83203d67a4e071448ab7778f
LeoNguyen-GH <leonguyen.ln1@gmail.com>
2024-07-09 22:04:32 +0100

    Update Get users' MFA method.ps1

--- Get users' MFA method.ps1
+++ Get users' MFA method.ps1
@@ -1,16 +1,14 @@
 . "$PSScriptRoot\common\functions.ps1"
 
 function get_users_data {
-    param (
-        [string[]]$OU_users = $null
-    )
+    param ([string[]]$OU_users = $null)
     
     $users_data = [System.Collections.Generic.List[PSCustomObject]]::new()
     
     $users = if ($OU_users) { Get-MsolUser -All | where-object { ($_.UserType -eq "Member") -and ($_.UserPrincipalName -in $OU_users) } } else { Get-MsolUser -All | where-object { ($_.UserType -eq "Member") -and ($_.BlockCredential -eq $false) -and ($_.IsLicensed -eq $true) } }
     
     foreach ($user in $users) {
-        $MFA_method = if ($user.StrongAuthenticationMethods) { $user.StrongAuthenticationMethods | Where-Object {$_.IsDefault -eq $true} | Select-Object -ExpandProperty MethodType } else { "Disabled" }
+        $MFA_method = if ($user.StrongAuthenticationMethods) { $user.StrongAuthenticationMethods | Where-Object {$_.IsDefault -eq $true} | Select-Object -ExpandProperty MethodType } else { "None" }
         
         $users_data.Add([PSCustomObject]@{
             "email" = $user.UserPrincipalName
@@ -28,11 +26,19 @@ function get_users_data {
     return $users_data
 }
 
+function get_OU_users {
+    param ([parameter(Mandatory)] [string]$ouDN)
+
+    try { return Get-ADUser -SearchBase $ouDN -Filter * | Select-Object -ExpandProperty UserPrincipalName } catch { write-host "Error: $_" -ForegroundColor Red }
+}
+
 function get_AD_users {
-    multi_user_input -prompt "Enter OU distinguished name to parse AD users (Newline for multiple, enter 'q' to continue):" | ForEach-Object {
-        $OU_users = Get-ADUser -SearchBase $_ -Filter * | Select-Object -ExpandProperty UserPrincipalName
+    $ouDNs = get_multi_user_input -prompt "Enter OU distinguished name to parse AD users (Newline for multiple, enter 'q' to continue):"
+    foreach ($ouDN in $ouDNs) {
+        if (!($OU_users = get_OU_users -ouDN $ouDN)) { continue }
+        
         $users = get_users_data -OU_users $OU_users
-        export_csv -csv_file_path "$PSScriptRoot\_logs\exported\MFA users status - $_ $(Get-Date -Format 'dd-MM-yyyy h.mmtt').csv" -data $users
+        export_csv -csv_file_path "$PSScriptRoot\_logs\exported\MFA users status - $ouDN $(Get-Date -Format 'dd-MM-yyyy h.mmtt').csv" -data $users
     }
 }
 
@@ -44,14 +50,11 @@ function get_entra_users {
 function main {
     if (closed_input -user_prompt "Do you want to parse users in an AD OU? (Y/N to get all users in Entra ID tenent)") {
         get_AD_users
-    }
-    else {
+    } else {
         get_entra_users
     }
 }
 
 MSOL_connect
 
-while ($true) {
-    main
-}
+while ($true) { main }
