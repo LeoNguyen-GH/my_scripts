7963c635e85aabf49925d1700eb02161cd0e757a
LeoNguyen-GH <leonguyen.ln1@gmail.com>
2024-07-09 21:53:54 +0100

    Update Get mailboxes forwarded to a user.ps1

--- Get mailboxes forwarded to a user.ps1
+++ Get mailboxes forwarded to a user.ps1
@@ -1,8 +1,8 @@
 . "$PSScriptRoot\common\functions.ps1"
 
-$all_mailboxes_csv_file_path = "$PSScriptRoot\_logs\exported\all mailboxes.csv"
+$cache_path = "$PSScriptRoot\_logs\cache\all mailboxes.csv"
 
-function populate_mailboxes {
+function export_all_mailboxes {
     $mailboxes = Get-Mailbox -ResultSize Unlimited | Select-Object UserPrincipalName, DisplayName, RecipientTypeDetails, ForwardingAddress, @{Name='ForwardingSmtpAddress'; Expression={$_.ForwardingSmtpAddress}}
 
     $mailboxes_data = [System.Collections.Generic.List[PSCustomObject]]::new()
@@ -20,7 +20,7 @@ function populate_mailboxes {
     return $mailboxes_data
 }
 
-function parse_csv {
+function search_csv {
     param(
         [parameter(Mandatory)] [string]$user,
         [parameter(Mandatory)] [PSCustomObject]$csv_data
@@ -44,23 +44,21 @@ function parse_csv {
 }
 
 function main {
-    $users = multi_user_input -prompt "Enter the user's email to find which mailboxes are being forwarded to it. (Newline for multiple, enter 'q' to continue):"
+    $users = get_multi_user_input -prompt "Enter the user's email to find which mailboxes are being forwarded to it. (Newline for multiple, enter 'q' to continue):"
     
-    if (closed_input -user_prompt "Do you want to repopulate the 'all mailboxes' csv file with all the mailboxes and respective fowarded email? (Y/N)" -y_msg "Repopulating 'all mailboxes' csv file.") {
-        $mailboxes_data = populate_mailboxes
-        export_csv -csv_file_path $all_mailboxes_csv_file_path -data $mailboxes_data
+    if (closed_input -user_prompt "Do you want to refresh the cache 'all mailboxes' csv file with all the mailboxes and respective fowarded emails? (Y/N)" -y_msg "Refreshing cache.") {
+        $mailboxes_data = export_all_mailboxes
+        export_csv -csv_file_path $cache_path -data $mailboxes_data
     }
-
-    $csv_data = Import-Csv -Path $all_mailboxes_csv_file_path
+    
+    $csv_data = Import-Csv -Path $cache_path
     
     foreach ($user in $users) {
-        $data = parse_csv -user $user -csv_data $csv_data
+        $data = search_csv -user $user -csv_data $csv_data
         export_csv -csv_file_path "$PSScriptRoot\_logs\exported\$user - Forwarded emails $(Get-Date -Format 'dd-MM-yyyy h.mmtt').csv" -data $data
     }
 }
 
 EXO_connect
 
-while ($true) {
-    main
-}
+while ($true) { main }
