6683ba9f6b15131c9db3c873d2e81af8e8e75d20
LeoNguyen-GH <leonguyen.ln1@gmail.com>
2024-07-11 22:14:40 +0100

    Update Get group or mailbox members.ps1

--- Get group or mailbox members.ps1
+++ Get group or mailbox members.ps1
@@ -9,7 +9,7 @@ function get_group_members {
             "Microsoft 365 group" {Get-UnifiedGroupLinks -Identity $group -LinkType Members -ResultSize Unlimited -ErrorAction SilentlyContinue | Select-Object @{Name='Name'; Expression={$_.DisplayName}}, @{Name='Email'; Expression={$_.PrimarySmtpAddress}}}
             "security group" {Get-AzureADGroupMember -ObjectId (Get-AzureADGroup -SearchString $group).ObjectId -All:$true -ErrorAction SilentlyContinue | Select-Object @{Name='Name'; Expression={$_.DisplayName}}, @{Name='Email'; Expression={$_.UserPrincipalName}}}
             "dynamic distribution list" { Get-Recipient -RecipientPreviewFilter (Get-DynamicDistributionGroup -Identity $group -ErrorAction SilentlyContinue).RecipientFilter | Select-Object @{Name='Name'; Expression={$_.DisplayName}}, @{Name='Email'; Expression={$_.PrimarySmtpAddress}} }
-            { $_ -in @("RoomMailbox", "shared mailbox", "user mailbox") } { Get-MailboxPermission -Identity (Get-Mailbox -Identity $group) | Where-object { $_.User -notlike "*SELF*" } | Select-Object @{Name='Name'; Expression={$_.User}},AccessRights }
+            { $_ -match "Mailbox" } { Get-MailboxPermission -Identity (Get-Mailbox -Identity $group) | Where-object { $_.User -notlike "*SELF*" } | Select-Object @{Name='Name'; Expression={$_.User}},AccessRights }
             default { throw "Could not find the group '$group'." }
         }
     } catch { write-warning $_ }
@@ -22,7 +22,7 @@ function main {
         if ($members = get_group_members -group $group) {
             export_csv -csv_file_path "$PSScriptRoot\_logs\exported\$group - list of members $(Get-Date -Format 'dd-MM-yyyy h.mmtt').csv" -data $members
         } else {
-            write-output "No members found for '$group'."
+            Write-Host "No members found for '$group'."
         }
     }
 }
