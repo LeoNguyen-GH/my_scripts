6f534a43bb9ba76c7c7f76d0f4fb83f170ceb5d1
LeoNguyen-GH <leonguyen.ln1@gmail.com>
2024-07-20 01:57:25 +0100

    Update Image file to JPG XL converter.py

--- Image file to JPG XL converter.py
+++ Image file to JPG XL converter.py
@@ -3,149 +3,205 @@ import subprocess
 from pathlib import Path
 import time
 from collections import Counter
-import shutil
 import uuid
 from datetime import datetime
 from common.functions import *
 
-while True:
-    while True:
-        user_input = input("Enter the directory path to convert image files to .jxl or input a number to automatically set path: ").strip().strip('"')
+def preset_paths(file_path):
+    with open(file_path, mode="r", encoding="utf-8") as file:
+        return file.read().splitlines()
         
-        directory_path = Path(user_input)
-
-        if not directory_path.exists() or not user_input:
-            printRed(f"The directory '{directory_path}' does not exist. Please enter a valid path.")
-            continue
-        printGreen(f"The directory '{directory_path}' has been validated.")
-
-        user_confirmation = user_choice("Is this the correct directory to convert images to JXL? (Y/N): ")
-        if user_confirmation is True:
-            break
-        printYellow("Input reset")
-
-    # Max number of images to process at the same time
-    while True:
-        max_workers = input("Input the number of max workers (Recommended number, # of CPU cores): ")
-        if max_workers.isdigit() and int(max_workers) > 0:
-            max_workers = int(max_workers)
-            break
-        printRed(f"Invalid input, try again")
-
-    # Calculate directory folder size
-    original_dir_size = dir_total_size(directory_path)
+def convert_to_dict(data_list):
+    data = {}
     
-    # Set up logging
-    log_file_current_date_time = datetime.now().strftime("Date=%Y-%m-%d & Time=%H.%M.%S")
-    log_dir = r"_Logs\Image to JXL converter"
-    logger = setup_logger(
-        log_path = fr"{log_dir}\{log_file_current_date_time}.log", 
-        error_log_path = fr"{log_dir}\{log_file_current_date_time} - error log.log",
-        logging_level_console = "INFO"
-    )
-    logging.getLogger('asyncio').disabled = True
-    # Temp directory name
-    temporary_dir_path = directory_path.joinpath("#Temp")
+    for index, item in enumerate(data_list):
+        data[index+1] = item
+    
+    return data
 
-    def process_image(file):
-        input_file = file
-        unique_id = str(uuid.uuid4())
-        source_file = temporary_dir_path.joinpath(file.stem + "_" + unique_id + file.suffix)
-        processed_file = source_file.with_suffix('.jxl')
+def display_options(options: dict):
+    for key, value in options.items():
+        print(f"{key}. {value}")
 
-        source_file.parent.mkdir(parents=True, exist_ok=True)
-        processed_file.parent.mkdir(parents=True, exist_ok=True)
+def dir_path_prompt(options: dict):
+    user_input = input("Enter in the directory path containing the images to convert to .jxl or input a number corresponding to the preset path: ").strip().strip('"')
+    return Path(options[int(user_input)]) if user_input in str(options) else Path(user_input)
 
-        # Log the original and temporary file names
-        logging.debug(f"'{input_file}' - Original File Name to '{source_file}' - Temporary File Name")
+def is_dir_path_valid(dir_path):
+    if not dir_path.exists() or not dir_path:
+        printRed(f"The directory '{dir_path}' could not be found."); return False
+    return True
 
-        # Move the image to the temporary directory
-        file.rename(source_file)
-
-        def reverse_process(original_file_path, file_path, remove_file, logs, extra_remove_file = None):
-            # Preserve the original file if error occured
-            logging.error(logs)
-            file_path.rename(original_file_path)
-            printYellow(f"Moving back '{original_file_path}' to original location")
-            # Removes cjxl processed processed_file file
-            if remove_file.exists():
-                remove_file.unlink()
-            if extra_remove_file and extra_remove_file.exists():
-                extra_remove_file.unlink()
-
-        try:
-            subprocess.run(["cjxl", str(source_file), "--distance", "0", "--effort", "10", str(processed_file)], stderr=subprocess.PIPE, check=True)
-        except (subprocess.CalledProcessError, Exception) as e:
-            reverse_process(input_file, source_file, processed_file, f"{input_file} - Error: {e}")
-            return
-        finally:
-            if not processed_file.exists():
-                reverse_process(input_file, source_file, processed_file, f"{input_file} - Procesed file not found")
-                return
-
-        # decode the processed file
-        decoded_processed_file = source_file.with_stem(source_file.stem + "_decoded")
-        subprocess.run(["djxl", str(processed_file), str(decoded_processed_file)], stderr=subprocess.PIPE, check=True)
-
-        # Compare decoded processed file and source file 
-        compare_cmd = ['magick', 'compare', '-metric', 'ae', str(source_file), str(decoded_processed_file), 'NUL']
-        compared_result = subprocess.run(compare_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-        # Move processed file back to source file directory if result returncode is 0
-        if compared_result.returncode == 0:
-            logging.info(f"'{input_file}' - Done: magick compare returned {compared_result.returncode}")
-            processed_file.rename(input_file.with_suffix('.jxl'))
-            # Removes the original image file and decoded processed file
-            decoded_processed_file.unlink()
-            source_file.unlink()
-
-            total_counter['processed'] += 1
-            print(f"Current images processed: {total_counter['processed']}/{total_counter['submitted']}")
-        else:
-            reverse_process(input_file, source_file, processed_file, f"{input_file} - Failed: magick compare returned {compared_result.returncode}", decoded_processed_file)
-
-    def filename_validation(filename):
-        try:
-            filename.encode('ascii')
-        except UnicodeEncodeError:
-            logging.warning(f"{file} - Skipped (contains non-ASCII characters)")
-            return False
-        else:
-            return True
+def get_dir_path(options: dict):
+    while True:
+        dir_path = dir_path_prompt(options)
         
+        if not is_dir_path_valid(dir_path):
+            continue
+        else:
+            if user_choice(f"Do you confirm the target directory '{dir_path}' is correct? (Y/N): "):
+                return dir_path
+
+def filename_validation(file_name, file_path):
+    try:
+        file_name.encode('ascii'); return True
+    except UnicodeEncodeError:
+        logging.warning(f"{file_path} - Skipped (contains non-ASCII characters)"); return False
+        
+def undo_process(original_file_path, file_path, remove_file, log_error):
+    # Preserve the original file if an error occured
+    logging.error(log_error)
+    printYellow(f"Moving file '{file_path}' back to its original location '{original_file_path}', removing file '{remove_file}'.")
+    
+    file_path.rename(original_file_path)
+    remove_file.unlink(missing_ok=True)
+    
+def convert_img(original_file_path, source_file_path, destination_file_path):
+    # Use CJXL to convert the moved image to a JXL image file
+    try:
+        subprocess.run(["cjxl", str(source_file_path), "--distance", "0", "--effort", "10", str(destination_file_path)], stderr=subprocess.PIPE, check=True)
+        if not destination_file_path.exists():
+            undo_process(original_file_path, source_file_path, destination_file_path, f"{original_file_path} - Processed file not found"); return False
+        return True
+    except (subprocess.CalledProcessError, Exception) as e:
+        undo_process(original_file_path, source_file_path, destination_file_path, f"{original_file_path} - Error: {e}"); return False
+
+def decode_and_compare_img(source_file_path, processed_file_path):
+    # decode the processed file
+    decoded_processed_file_path = source_file_path.with_stem(source_file_path.stem + "_decoded")
+    subprocess.run(["djxl", str(processed_file_path), str(decoded_processed_file_path)], stderr=subprocess.PIPE, check=True)
+
+    # Compare decoded processed file and source file 
+    compared_result = subprocess.run(['magick', 'compare', '-metric', 'ae', str(source_file_path), str(decoded_processed_file_path), 'NUL'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    decoded_processed_file_path.unlink()
+    
+    return compared_result
+
+def process_result(result, file_path, source_file_path, processed_file_path):
+    if result.returncode == 0:
+        # Move processed file back to source file directory and remove the source file
+        logging.info(f"'{file_path}' - Done: magick compare returned {result.returncode}")
+        
+        processed_file_path.rename(file_path.with_suffix('.jxl'))
+        
+        source_file_path.unlink()
+        
+        global img_counter; img_counter['processed'] += 1
+        print(f"Current images processed: {img_counter['processed']}/{img_counter['submitted']}")
+    else:
+        undo_process(file_path, source_file_path, processed_file_path, f"{file_path} - Failed: magick compare returned {result.returncode}")
+
+def process_image(file_path, temp_dir_path):
+    temp_dir_path.mkdir(parents=True, exist_ok=True)
+    
+    source_file_path = temp_dir_path.joinpath(file_path.stem + "_" + str(uuid.uuid4()) + file_path.suffix)
+    processed_file_path = source_file_path.with_suffix('.jxl')
+
+    logging.debug(f"'{file_path}' - Original File Name to '{source_file_path}' - Temporary File Name")
+
+    # Move the image to the temporary directory. 
+    file_path.rename(source_file_path) # Note: images are moved to the temp directory because CJXL is unable to handle certain characters if present in the file's path
+    
+    if not convert_img(file_path, source_file_path, processed_file_path):
+        return
+
+    compared_result = decode_and_compare_img(source_file_path, processed_file_path)
+    
+    process_result(compared_result, file_path, source_file_path, processed_file_path)
+
+def total_time_taken():
     start_time = time.time()
-
-    total_counter = Counter()
-    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-        for file in directory_path.glob('**/*'):
-            if  file.is_file() and file.suffix.lower() in ('.png', '.jpg', '.jpeg') and filename_validation(file.name) is True:
-                total_counter['submitted'] += 1
-                executor.submit(process_image, file)
-
-    # Remove the temporary directory if it exists
-    if temporary_dir_path.exists():
-        shutil.rmtree(temporary_dir_path)
-
+    yield
+    
     elapsed_time = time.time() - start_time
     hours, remainder = divmod(elapsed_time, 3600)
     minutes, seconds = divmod(remainder, 60)
+    
+    yield hours, minutes, seconds
+    
+def start_executor(dir_path, function, max_workers, temp_dir_path):
+    global img_counter
+    futures = []
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+        for file in list(dir_path.glob('**/*')): # Converting to a list so all files are collected at once, instead of yielding one at a time
+            if file.is_file() and file.suffix.lower() in ('.png', '.jpg', '.jpeg') and filename_validation(file.name, file) and file.parent != temp_dir_path:
+                img_counter['submitted'] += 1
+                futures.append(executor.submit(function, file, temp_dir_path))
+                
+        concurrent.futures.wait(futures)
 
+def calculate_space_saved(dir_path):
+    original_dir_size = dir_total_size(dir_path)
+    yield
+    
     # Calculate directory size after processing
-    processed_dir_size = dir_total_size(directory_path)
+    processed_dir_size = dir_total_size(dir_path)
+    
     # Calculate size difference between original and processed directory
     size_diff = bytes_unit_conversion(original_dir_size - processed_dir_size)
+    
     # Calculate the difference between original and process directory in percentage terms
-    percentage_processed_to_original = (processed_dir_size / original_dir_size) * 100
-    percentage_processed_to_original = f"{percentage_processed_to_original:.3f}%"
+    percentage_processed_to_original = f"{(processed_dir_size / original_dir_size) * 100:.3f}%"
+    
+    yield size_diff, percentage_processed_to_original
 
-    logging.info(f"Total images submitted: {total_counter['submitted']}, processed: {total_counter['processed']}")
-    logging.info(f"Total space saved: {size_diff} ({percentage_processed_to_original} of original size)")
-    logging.info(f"Total elapsed time: {int(hours)} hours, {int(minutes)} minutes, {int(seconds)} seconds")
-
-    # close logging handler
+def release_logger_handlers(logger):
     for handler in logger.handlers[:]:
         logger.removeHandler(handler)
         handler.close()
+            
+def main():
+    create_text_file(preset_paths_text_file_path)
+    
+    lines = preset_paths(preset_paths_text_file_path)
+    options = convert_to_dict(lines)
+    
+    display_options(options)
+    
+    dir_path = get_dir_path(options)
+    
+    max_workers = num_input(
+        prompt = "Input the amount of images to process in parallel (Must be greater than 0): ",
+        condition = lambda x: x > 0
+    )
+    
+    log_dir = Path.home().joinpath(r"Documents\_Logs\Image to JXL converter"); current_date_time = datetime.now().strftime("Date=%Y-%m-%d & Time=%H.%M.%S")
+    logger = setup_logger(
+        log_path = fr"{log_dir}\{current_date_time}.log", 
+        error_log_path = fr"{log_dir}\{current_date_time} - error log.log",
+        logging_level_console = "INFO"
+    )
+    logging.getLogger('asyncio').disabled = True
+    
+    space_saved = calculate_space_saved(dir_path); next(space_saved)
+        
+    timer = total_time_taken(); next(timer)
+    
+    global img_counter
+    img_counter = Counter({"submitted": 0, "processed": 0})
+    temp_dir_path = dir_path.joinpath("_temp")
+    
+    start_executor(dir_path, process_image, max_workers, temp_dir_path)
+    
+    temp_dir_path.rmdir()
+    
+    hours, minutes, seconds = next(timer)
 
-    purge_empty_files(log_dir)
+    size_diff, percentage = next(space_saved)
+
+    logging.info(f"Total images submitted: {img_counter['submitted']}, processed: {img_counter['processed']}")
+    logging.info(f"Total space saved: {size_diff} ({percentage} of original size)")
+    logging.info(f"Total elapsed time: {int(hours)} hours, {int(minutes)} minutes, {int(seconds)} seconds")
+
+    release_logger_handlers(logger)
+
+    remove_empty_text_files(log_dir)
     print_dash_across_terminal()
+
+if __name__ == "__main__":
+    preset_paths_text_file_path = f"{__file__} - Preset directories.txt"
+    
+    while True:
+        main()
+        
