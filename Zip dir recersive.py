084b559cec68488fb5ae4ed474b598c0d1255890
LeoNguyen-GH <leonguyen.ln1@gmail.com>
2024-07-28 01:46:53 +0100

    Update Zip dir recersive.py

--- Zip dir recersive.py
+++ Zip dir recersive.py
@@ -2,59 +2,74 @@ import subprocess
 import shutil
 from pathlib import Path
 from datetime import datetime
-import time
+from tqdm import tqdm
 from common.functions import *
+from image_to_JPG_XL_converter import preset_paths, convert_to_dict, display_options, get_dir_path, total_time_taken
 
-while True:
-    while True:
-        user_input = input("Enter the directory path to zip folders: ").strip('"').strip("'")
-        
-        source_dir = Path(user_input)
-        if not source_dir.exists() or not user_input:
-            printRed(f"Directory '{user_input}' not found.")
-            continue
-        else:
-            printGreen(f"Directory '{user_input}' found.")
-
-        user_confirmation = user_choice("Confirm directory to continue. (Y/N): ")
-        if user_confirmation is True:
-            break
-        else:
-            printYellow("Directory path reset.")
-
-    current_time = datetime.now().strftime("Date=%Y-%m-%d & Time=%H.%M.%S")
-    log_file = Path(fr"_Logs\7-Zip\7-Zip log - {current_time}.log")
-    logger = setup_logger(log_path = log_file)
-
-    start_time = time.time()
-    total_folders = 0
-    total_folders_zipped = 0
-
-    for root, dirs, files in Path.walk(source_dir, top_down=False):  
-        for file in files:
-            file_path = root / file
-            file_rel_path = file_path.parent
-            zip_filename = Path(str(file_rel_path) + ".cbz")
-
-            # Skip processing files in source_dir
-            if file_rel_path != source_dir and file_rel_path.parent != source_dir and not zip_filename.exists():
-                total_folders += 1
-                logging.info(f"Zipping '{file_rel_path}' -> '{zip_filename}'")
-                process = subprocess.run(["7z", "a", "-r", "-mx0", "-tzip", zip_filename, str(file_rel_path)], stdout=subprocess.PIPE)
-                output = process.stdout
-                
-                if Path(file_rel_path).exists() and Path(zip_filename).exists():
-                    shutil.rmtree(file_rel_path)
-                    total_folders_zipped += 1
-                elif not Path(file_rel_path).exists():
-                    logging.error(f"{zip_filename} - Failed to create zip file")
-
-                logging.info(f"Current folders zipped: {total_folders_zipped}/{total_folders}.") 
+def zip_dir(source_dir_path, destination_zip_file_path):
+    global folders; folders["submitted"] += 1
     
-    # Calculate total elapsed time
-    elapsed_time = time.time() - start_time
-    hours, remainder = divmod(elapsed_time, 3600)
-    minutes, seconds = divmod(remainder, 60)
+    logging.debug(f"Zip: '{source_dir_path}' -> '{destination_zip_file_path}'")
+    process = subprocess.run(["7z", "a", "-r", "-mx0", "-tzip", destination_zip_file_path, source_dir_path], stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    
+    if process.returncode != 0:
+        logging.error(f"stderr: {process.stderr.decode('utf-8')}")
 
-    logging.info(f"Processed {total_folders} folders, zipped {total_folders_zipped} subdirectories.")
-    logging.info(f"Total elapsed time: {int(hours)} hours, {int(minutes)} minutes, {int(seconds)} seconds.")
+def validate_and_cleanup(dir_path, zip_file_path):
+    global folders
+    
+    if zip_file_path.exists() and dir_path.exists():
+        folders["zipped"] += 1
+        shutil.rmtree(dir_path)
+    elif not zip_file_path.exists():
+        folders["errored"] += 1
+        logging.error(f"{zip_file_path} - Failed to create zip file")
+
+def process_dir(dir_path):
+    dir_to_process = {}
+    
+    for root, dirs, files in Path(dir_path).walk(top_down=False):
+        for dir in dirs:
+            subdir_path = root / dir
+            zip_file_path = Path(f"{subdir_path}.zip")
+            
+            # Exludes the folders 1 level down from the root directory path 
+            if subdir_path.parent != dir_path:
+                dir_to_process[subdir_path] = zip_file_path
+    
+    with tqdm(total=len(dir_to_process), unit="dirs") as pbar:
+        for subdir_path, zip_file_path in dir_to_process.items():
+            zip_dir(subdir_path, zip_file_path)
+            validate_and_cleanup(subdir_path, zip_file_path)
+            pbar.update()
+
+def main():
+    create_text_file(preset_paths_text_file_path)
+    
+    lines = preset_paths(preset_paths_text_file_path)
+    options = convert_to_dict(lines)
+    
+    display_options(options)
+    
+    dir_path = get_dir_path(options, "Enter in the directory path containing the folders to zip or input a number corresponding to the preset path: ")
+
+    log_dir = Path.home().joinpath(r"Documents\_Logs\7-Zip")
+    current_time = datetime.now().strftime("Date=%Y-%m-%d & Time=%H.%M.%S")
+    logger = setup_logger(log_path=fr"{log_dir}\7-Zip log - {current_time}.log", logging_level_console="INFO")
+    
+    timer = total_time_taken(); next(timer)
+    
+    global folders; folders = {"submitted": 0, "zipped": 0, "errored": 0}
+    
+    process_dir(dir_path)
+    
+    hours, minutes, seconds = next(timer)
+
+    logging.info(f"Total folders submitted: {folders['submitted']} , zipped: {folders['zipped']}")
+    logging.info(f"Total elapsed time: {hours} hours, {minutes} minutes, {seconds} seconds")
+    
+if __name__ == "__main__":
+    preset_paths_text_file_path = f"{__file__} - Preset directories.txt"
+    
+    while True:
+        main()
