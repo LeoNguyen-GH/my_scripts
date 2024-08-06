295298490e2e30e5c8eb2447080d8d92c563180e
LeoNguyen-GH <leonguyen.ln1@gmail.com>
2024-08-07 00:02:26 +0100

    Update Get dir size and number of files.py

--- Get dir size and number of files.py
+++ Get dir size and number of files.py
@@ -1,85 +1,84 @@
 from common.functions import *
+from image_to_JPG_XL_converter import total_time_taken
 import concurrent.futures
 from pathlib import Path
-import queue
 import time
+import threading
+import re
 
-while True:
-    user_input = input("Input directory name: ").strip()
-    if not user_input:
-        continue
-    disks = 7
-    files, files_total_byte_size = queue.Queue(), queue.Queue()
-    files_total_byte_size.put(0)
-    bg_display_stop = 0
+def increment_counter(key: str, value: int=1):
+    global counter
+    with lock:
+        counter[key] += value
+        
+def format_dir(dir):
+    return re.sub(r"([A-Za-z]:\\)(.*)", r'\2', dir)
+
+def get_available_dirs(total_disks: int, dir):
+    # Gets all the available directories in the found disk shares
+    available_dirs = []
     
-    def update_files_total_byte_size(size):
-        current_size = files_total_byte_size.get()
-        new_size = current_size + size
-        files_total_byte_size.put(new_size)
+    for i in range(1, total_disks+1):
+        formatted_dir = Path(re.sub(r"(\\\\[0-9.]+\\)(disk)(.*)", fr'\1\g<2>{i}\3', str(dir)))
+        if formatted_dir.exists():
+            available_dirs.append(formatted_dir)
     
-    def available_disks(append_dir = None):
-        source_dirs = []
-        for i in range(1, disks+1):
-            source_dir = Path(fr"\\192.168.11.1\disk{i}\Backup\{append_dir}") if append_dir else Path(fr"\\192.168.11.1\disk{i}\Backup")
-            if source_dir.exists():
-                source_dirs.append(source_dir)
-        return source_dirs
-    
-    def submit_executor(function, item_list):
-        with concurrent.futures.ThreadPoolExecutor(max_workers=disks) as executor:
-            futures = [executor.submit(function, item) for item in item_list]
-            executor.submit(bg_display(len(item_list)))
+    return available_dirs
+
+def display_counter(interval: int|float=0.5):
+    global counter
+    global display_counter_status
+    while display_counter_status:
+        print(f"Total files: {counter['total files']}, size: {bytes_unit_conversion(counter['files total byte size'])} ({counter['files total byte size']})", end='\r')
+        time.sleep(interval)
+
+def dir_executor(dir_path):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=3000) as executor:
+        for root, dirs, files in dir_path.walk():
+            futures = [executor.submit(increment_total_file_and_size, root / file) for file in files]
             concurrent.futures.wait(futures)
-    
-    def file_executor(directory):
-        global bg_display_stop
-        with concurrent.futures.ThreadPoolExecutor(max_workers=3000) as executor:
-            for root, dirs, files in directory.walk():
-                file_futures = [executor.submit(file_path_and_size, root, file) for file in files]
-        concurrent.futures.wait(file_futures)
-        bg_display_stop += 1
-    
-    def file_path_and_size(root, file):
-        file_path = root.joinpath(file)
-        files.put(file_path)
-        update_files_total_byte_size(file_path.stat().st_size)
-    
-    def items_per_second():
-        start_time = time.time()
-        start_num_items = int(files.qsize())
-        while True:
-            elapsed_time = time.time() - start_time
-            end_num_items = int(files.qsize()) - start_num_items
-            if elapsed_time >= 1:
-                items_per_second = end_num_items / elapsed_time
-                print("Items added per second:", items_per_second)
-                start_time = time.time()
-                start_num_items = int(files.qsize())
 
-    def bg_display(stop, polling_period = 0.5):
-        global bg_display_stop
-        prev_total_files = 0
-        while bg_display_stop != stop:
-            current_total_files = files.qsize()
-            total_files_added = current_total_files - prev_total_files
-            total_files_per_second = int((1/polling_period)*total_files_added)
-            prev_total_files = current_total_files
-            total_size = files_total_byte_size.queue[0]
-            print(f"Total files: {current_total_files}, {total_files_per_second}it/s, Total size: {bytes_unit_conversion(total_size)} ({total_size:,} bytes)", end='\r')
-            time.sleep(polling_period)
+def increment_total_file_and_size(file_path):
+    increment_counter("total files")
+    increment_counter("files total byte size", file_path.stat().st_size)
 
-    start_time = time.time()
+def main():
+    unraid_array_total_disks = 5
+    unraid_server_ip = "192.168.0.2"
+    unraid_user_share = "share1"
+    global counter; counter = {
+        "files total byte size": 0,
+        "total files": 0
+    }
     
-    disk_share_paths = available_disks(user_input)
-    submit_executor(
-        function = file_executor, 
-        item_list = disk_share_paths
-    )
+    user_input = input(r"Input the UNRAID directory path to parse its total files and size (e.g. P:\folder1, folder1): ").strip(' "')
+    formatted_dir = format_dir(user_input)
+    fully_formatted_dir = Path(fr"\\{unraid_server_ip}\disk\{unraid_user_share}") / formatted_dir
     
-    files, files_total_byte_size = list(files.queue), int(files_total_byte_size.get())
+    disk_share_dirs = get_available_dirs(total_disks=unraid_array_total_disks, dir=fully_formatted_dir)
     
-    converted_bytes = bytes_unit_conversion(files_total_byte_size)
-    print(f"Total size of 'P:\\{user_input}': {converted_bytes} ({files_total_byte_size:,} bytes)")
-    print(f"Total files: {len(files)}")
-    print(f"Time elapsed: {elapsed_time(time.time() - start_time)}")
+    timer = total_time_taken()
+    next(timer)
+    
+    global display_counter_status; display_counter_status = True
+    thread = threading.Thread(target=display_counter, args=(0.5,))
+    thread.start()
+    
+    with concurrent.futures.ThreadPoolExecutor(max_workers=unraid_array_total_disks) as executor:
+        futures = [executor.submit(dir_executor, dir) for dir in disk_share_dirs]
+        concurrent.futures.wait(futures)
+    
+    display_counter_status = False
+    thread.join()
+    
+    hours, minutes, seconds = next(timer)
+    formatted_time_taken = f"{hours}h {minutes}mins {seconds}s"
+    
+    converted_bytes = bytes_unit_conversion(counter["files total byte size"])
+    
+    print(f"Total files: {counter['total files']}, size: {converted_bytes} ({counter['files total byte size']})")
+    print(f"Elapsed time: {formatted_time_taken}")
+    
+if __name__ == "__main__":
+    lock = threading.Lock()
+    main()
